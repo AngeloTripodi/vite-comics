c570fee8f2dce40fa1b256ce228fafad2dad0615
AngeloTripodi <tripodi.angelo@gmail.com>
2023-01-09 17:48:07 +0100

    Create Footer

--- src/components/MainFooter.vue
+++ src/components/MainFooter.vue
@@ -5,11 +5,42 @@ export default {
 </script>
 
 <template>
-
-    <h1>UEss!</h1>
-
+    <footer>
+        <section class="wrapper">
+            <div class="links">
+                <div class="link1">
+                    a
+                </div>
+                <div class="link2">
+                    a
+                </div>
+                <div class="link3">
+                    a
+                </div>
+            </div>
+            <div class="imgBackground">
+                <img src="../assets/img/dc-logo-bg.png" alt="">
+            </div>
+        </section>
+    </footer>
 </template>
 
 <style lang="scss">
+footer {
+    background-image: url(../assets/img/footer-bg.jpg);
+    background-position: center;
+    background-repeat: no-repeat;
+    background-size: cover;
+    padding: 2rem 0;
+    overflow: hidden;
+}
 
+.wrapper {
+    display: flex;
+    justify-content: space-between;
+}
+
+.imgBackground img {
+    margin: -130px;
+}
 </style>
