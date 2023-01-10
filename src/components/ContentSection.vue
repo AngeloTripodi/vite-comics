2548e4470b34c10db915d7cf8dd21c16c3fd64d8
AngeloTripodi <tripodi.angelo@gmail.com>
2023-01-10 18:55:50 +0100

    Add Button

--- src/components/ContentSection.vue
+++ src/components/ContentSection.vue
@@ -18,12 +18,17 @@ export default {
 
 <template>
     <section class="contentSection">
+        <div class="currentCategories wrapper">
+            <div class="currentElement">CURRENT SERIES</div>
+        </div>
         <div class="content wrapper">
             <div class="comicElement" v-for="comic in comicsList">
                 <CardComponent :imgPath="comic.thumb" :comicsType="comic.type" :comicsSeries="comic.series"
                     :price="comic.price" />
             </div>
-
+        </div>
+        <div class="buttonSection wrapper">
+            <button id="loadMore">LOAD MORE</button>
         </div>
     </section>
 </template>
@@ -33,7 +38,7 @@ export default {
     background-color: #1c1c1c;
 
     .content {
-        padding: 2rem 0;
+        padding: 4rem 0 2rem 0;
         color: white;
         display: flex;
         flex-wrap: wrap;
@@ -47,5 +52,42 @@ export default {
         margin: 0 auto;
         padding: 0 5px;
     }
+
+
+    .buttonSection {
+        display: flex;
+        justify-content: center;
+
+        & #loadMore {
+            font-weight: bold;
+            color: white;
+            background-color: #1374ed;
+            border: none;
+            padding: .75rem 3rem;
+            margin: .5rem 0 1.5rem 0;
+        }
+
+    }
+
+    .currentCategories {
+        max-width: 1230px;
+    }
+
+    .currentElement {
+        position: absolute;
+        top: 512px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        width: 225px;
+        height: 50px;
+        text-align: center;
+        font-size: 2rem;
+        font-weight: bold;
+        color: white;
+        background-color: #1374ed;
+    }
+
+
 }
 </style>
